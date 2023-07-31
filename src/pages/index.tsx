d6dce00aa1c02caa3e137c06c794bf42758f5066
Andres Aguilar <aaguilarc@uach.mx>
2023-07-31 04:05:08 -0500

    PORFIIIIIIIIIN! Ya listo pa treparlo al hackathon

---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import styles from "@/styles/Home.module.css";
+import { encodeURL } from "@solana/pay";
+import { Keypair } from "@solana/web3.js";
+import { useMemo, useState } from "react";
+import QRCode from "react-qr-code";
+
+const Home = () => {
+  const [qrCode, setQrCode] = useState<string | null>(null);
+  const [quantity, setQuantity] = useState(0);
+  const reference = useMemo(() => Keypair.generate().publicKey, []);
+
+  const createPayment = async () => {
+    if (!quantity) {
+      return;
+    }
+
+    const apiUrl = `${process.env.NEXT_PUBLIC_APP_URL}/api/makeTransaction?amount=${quantity}&reference=${reference}`;
+
+    const urlParams = {
+      link: new URL(apiUrl),
+      label: "Superteam MX Coincha Redeem",
+      message: "Thanks for buying our coinchas!",
+    };
+    const solanaUrl = encodeURL(urlParams);
+    setQrCode(solanaUrl.href);
+  };
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        />
+        <button onClick={createPayment}>Generate QR</button>
+      </div>
+
+      {qrCode && <QRCode value={qrCode} />}
+    </main>
+  );
+};
+
+export default Home;

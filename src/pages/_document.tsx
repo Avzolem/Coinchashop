a397b7f846bff414224f46ebff712ab844fad6c1
Andres Aguilar <aaguilarc@uach.mx>
2023-07-31 10:00:19 -0600

    Footer, Socials, quantity fix, placeholders, title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" >
-      <Head />
+      <Head>
+                <link rel="icon" href="/favicon.ico" />
+                
+                <meta
+                    name="description"
+                    content="Coincha Token Shop"
+                />
+            </Head>
       <body>
         <Main />
         <NextScript />
